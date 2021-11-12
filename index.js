a172e75c28724118625ae5ec771254fe02703c58
Ab <computerm246@gmail.com>
2021-11-13 01:51:39 +0600

    heroku setup

--- index.js
+++ index.js
@@ -21,15 +21,34 @@ async function run() {
         await client.connect();
         const database = client.db("luxury_car");
         const sliderData = database.collection("slider_data");
+        const products_collection = database.collection("products_collection");
+        const orders_collection = database.collection("orders_collection");
 
         // get slider data
-
         app.get("/slider_data", async (req, res) => {
             const result = await sliderData.find({}).toArray();
             res.json(result);
         });
 
+        // get products data
+        app.get("/products_data", async (req, res) => {
+            const result = await products_collection.find({}).toArray();
+            res.json(result);
+        });
 
+        // get a specific product data
+        app.get("/:product_id", async (req, res) => {
+            const id = req.params.product_id;
+            const result = await products_collection.findOne({ _id: ObjectId(id) });
+            res.json(result);
+        });
+    
+    // add to orders_collection
+    app.post("/add_to_product", async (req, res) => {
+        const data = req.body;
+        const result = await orders_collection.insertOne(data);
+        res.json(result);
+    });
     }
 
     finally {
