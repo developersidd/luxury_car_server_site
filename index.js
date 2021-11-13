80b274e79384fa0266b56ef993f372bc8456af41
Ab <computerm246@gmail.com>
2021-11-13 10:40:58 +0600

    add user info to db

--- index.js
+++ index.js
@@ -23,6 +23,7 @@ async function run() {
         const sliderData = database.collection("slider_data");
         const products_collection = database.collection("products_collection");
         const orders_collection = database.collection("orders_collection");
+        const user_data = database.collection("user_data");
 
         // get slider data
         app.get("/slider_data", async (req, res) => {
@@ -42,13 +43,27 @@ async function run() {
             const result = await products_collection.findOne({ _id: ObjectId(id) });
             res.json(result);
         });
-    
-    // add to orders_collection
-    app.post("/add_to_product", async (req, res) => {
-        const data = req.body;
-        const result = await orders_collection.insertOne(data);
-        res.json(result);
-    });
+
+        // add to orders_collection
+        app.post("/add_to_product", async (req, res) => {
+            const data = req.body;
+            const result = await orders_collection.insertOne(data);
+            res.json(result);
+        }); 1
+
+        // add register user data to db
+        app.post("/add_user_data", async (req, res) => {
+            const data = req.body;
+            const result = await user_data.insertOne(data);
+            res.json(result);
+        });
+
+//        // add google user data to db
+//        app.put("/add_google_user_data", (req, res) => {
+//
+//        })
+
+
     }
 
     finally {
