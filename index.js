9d99127d6fa47e9f67578f99a8dd17800ba3d287
AB Siddik <siddik.prgmr@gmail.com>
2023-01-19 11:44:10 +0600

    created new repository and modified

--- index.js
+++ index.js
@@ -1,27 +1,38 @@
+/* 
+ ==> Title: Main File of Luxury Car website's Server Side Project
+ ==> Author: AB Siddik
+ ==> Date: First Development: 24/11/2021. Modified on: 1/18/2023
+*/
+
+// Dependencies
 const express = require("express");
 const app = express();
 const cors = require("cors");
 require("dotenv").config();
+const { MongoClient } = require('mongodb');
 const ObjectId = require("mongodb").ObjectId;
-const port = process.env.PORT || 5000;
 const fileUpload = require("express-fileupload");
 
-// middleware
+// Environment Variables
+const port = process.env.PORT || 5000;
+
+// middlewares
 app.use(express.json());
 app.use(cors());
 app.use(fileUpload());
 
-const { MongoClient } = require('mongodb');
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.minbj.mongodb.net/myFirstDatabase?
-r1etryWrites=false&w=majority`;
+// connection url of MongoDB Database
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.minbj.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
+// Main Function
 async function run() {
 
     try {
+
+        // connecting with MongoDB Database
         await client.connect();
         const database = client.db("luxury_car");
-        const sliderData = database.collection("slider_data");
         const products_collection = database.collection("products_collection");
         const orders_collection = database.collection("orders_collection");
         const user_data = database.collection("user_data");
@@ -32,7 +43,6 @@ async function run() {
         app.get("/get_review", async (req, res) => {
             let result = await review.find({}).toArray();
             res.json(result);
-            console.log(result)
         });
 
         // get all the user info to check admin role
@@ -42,7 +52,7 @@ async function run() {
             let isAdmin = false;
             if (result?.role === "admin") {
                 isAdmin = true;
-            }
+            };
             res.json({ admin: isAdmin });
         });
 
@@ -58,12 +68,6 @@ async function run() {
         app.get("/orders", async (req, res) => {
             const email = req.query.email;
             let result = await orders_collection.find({}).toArray()
-            res.json(result);
-        });
-
-        // get slider data
-        app.get("/slider_data", async (req, res) => {
-            const result = await sliderData.find({}).toArray();
             res.json(result);
         });
 
@@ -88,7 +92,7 @@ async function run() {
             const convertString = files.toString("base64");
             const imageBuffer = Buffer.from(convertString, "base64");
             const { name, description, price, fuel, condition, cc } = data;
-            const newPd = { image: imageBuffer, name, description, price, fuel, condition, cc};
+            const newPd = { image: imageBuffer, name, description, price, fuel, condition, cc };
             const result = await products_collection.insertOne(newPd);
             res.json(result);
         });
@@ -115,7 +119,6 @@ async function run() {
             res.json(result);
         });
 
-
         // add google user data to db
         app.put("/add_user_data", async (req, res) => {
             const user = req.body;
@@ -137,17 +140,31 @@ async function run() {
     }
 
     finally {
-
     }
-
-
 }
 
-run().catch(console.dir());
+run().catch((err) => console.log(err));
 
 app.get("/", (req, res) => {
     res.send("Running Luxury Car");
 });
+
+// 404 Error Handler
+app.use((req, res, next) => {
+    const error = new Error("The requested page does not exist!");
+    error.status = 404;
+    res.status(error.status).json(error.message);
+});
+
+//Default Error Handler
+app.use((err, req, res, next) => {
+    if (res.headersSent) {
+        next(err);
+    };
+    res.status(err.status || 500).json(err.message || "There was an Error");
+});
+
+
 
 app.listen(port, () => {
     console.log("Running project at", port);
